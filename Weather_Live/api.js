77e8d75c06f6a780868f19f2f8a44e7bb4302d87
BhriguKD <vrigukumar710@gmail.com>
2025-07-25 14:30:19 +0530

    Added API module

---
+++ Weather_Live/api.js
@@ -0,0 +1,23 @@
+const API_KEY = 'oooo'
+
+export async function getWeatherByCity(city) {
+  const currWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
+
+  try {
+    const [currWeatherResponse, forecastResponse] = await Promise.all([fetch(currWeatherUrl), fetch(forecastUrl)])
+
+    if(!currWeatherResponse.ok || !forecastResponse){
+      throw new Error('City not found or API error')
+    }
+
+  const currentWeatherData = await currWeatherResponse.json()
+  const forecastData = await forecastResponse.json()
+
+  return {current: currentWeatherData, forecast: forecastData}
+
+  } catch(error) {
+    console.log("Failed to fetch weather data: ", error)
+    throw error
+  }
+}
